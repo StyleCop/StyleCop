6a8bd38e128f8e9421173ad534183ba0b869c388
Jason Allor <jason.allor@microsoft.com>
2010-06-30 11:40:48 -0700

    Changed version to 4.4.0.12

--- Project/Src/AssemblyVersion.cs
+++ Project/Src/AssemblyVersion.cs
@@ -14,4 +14,4 @@
 //-----------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.4.0.11")]
+[assembly: AssemblyVersion("4.4.0.12")]
