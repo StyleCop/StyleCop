5ac89ef522647f893966a69d73048759cb5051e5
Andy Reeves <devnull@localhost>
2011-03-15 22:57:56 +0000

    Version updated to 4.5.0.1

--- Project/Src/AssemblyVersion.cs
+++ Project/Src/AssemblyVersion.cs
@@ -14,4 +14,4 @@
 //-----------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.5.0.1")]
