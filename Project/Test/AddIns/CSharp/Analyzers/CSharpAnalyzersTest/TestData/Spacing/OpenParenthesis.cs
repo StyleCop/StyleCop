8ccf5b39891494116ac6f8047565a25d7f457b10
Andy Reeves <devnull@localhost>
2011-03-17 21:51:24 +0000

    Adding more test cases.

--- Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/Spacing/OpenParenthesis.cs
+++ Project/Test/AddIns/CSharp/Analyzers/CSharpAnalyzersTest/TestData/Spacing/OpenParenthesis.cs
@@ -11,5 +11,24 @@ namespace CSharpAnalyzersTest.TestData.Spacing
         {
             M(a: () => Console.WriteLine());
         }
+
+        public void Method1()
+        {
+            this.Foo1(param: (object)null);
+            this.Foo2(param: (int x) => { return x * x; });
+            this.Foo3(param: () => { });
+        }
+
+        public void Foo1(object param)
+        {
+        }
+
+        private void Foo2(Func<int, int> param)
+        {
+        }
+
+        private void Foo3(Action param)
+        {
+        }
    }
 }
