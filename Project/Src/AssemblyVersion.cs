fb2488d11e95288c3b71fb4e7e1dd3e987f6cb05
Andy Reeves <devnull@localhost>
2011-03-24 23:40:51 +0000

    Updating build number.

--- Project/Src/AssemblyVersion.cs
+++ Project/Src/AssemblyVersion.cs
@@ -14,4 +14,4 @@
 //-----------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.5.0.9")]
+[assembly: AssemblyVersion("4.5.0.10")]
