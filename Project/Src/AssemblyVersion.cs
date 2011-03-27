8dc9c28525d572bdf536fd0e6b02e096df0a7428
Andy Reeves <devnull@localhost>
2011-03-27 13:15:48 +0100

    Updting Installer to remove previous versions of StyleCop4.2 or greater.

--- Project/Src/AssemblyVersion.cs
+++ Project/Src/AssemblyVersion.cs
@@ -14,4 +14,4 @@
 //-----------------------------------------------------------------------
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.5.0.10")]
+[assembly: AssemblyVersion("4.5.1.0")]
