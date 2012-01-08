43579556a5e93baa10c55234f4fc2e6658267ed8
Andy Reeves <devnull@localhost>
2012-01-08 23:48:54 +0000

    Fix for 7105.

--- Project/Src/AddIns/ReSharper61/CodeCleanup/Rules/DocumentationRulesConfiguration.cs
+++ Project/Src/AddIns/ReSharper61/CodeCleanup/Rules/DocumentationRulesConfiguration.cs
@@ -20,12 +20,27 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
 {
     #region Using Directives
 
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Security.Permissions;
+    using System.Text;
+
+    using JetBrains.ProjectModel;
+    using JetBrains.ReSharper.Feature.Services.CSharp.CodeCleanup;
+    using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+    using JetBrains.ReSharper.LiveTemplates;
+    using JetBrains.ReSharper.LiveTemplates.CSharp.Macros;
     using JetBrains.ReSharper.Psi;
+    using JetBrains.ReSharper.Psi.CSharp;
 
     using StyleCop.ReSharper61.Core;
 
     #endregion
-
+    
     /// <summary>
     /// A class that exposes the current Documentation configuration for the file provided.
     /// </summary>
@@ -36,6 +51,8 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
         private const string AnalyzerName = "StyleCop.CSharp.DocumentationRules";
 
         private readonly Settings settings;
+
+        private readonly IPsiSourceFile file;
 
         #endregion
 
@@ -50,6 +67,8 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
         public DocumentationRulesConfiguration(IPsiSourceFile file)
         {
             this.settings = new StyleCopSettings(StyleCopCoreFactory.Create()).GetSettings(file.ToProjectFile());
+
+            this.file = file;
 
             // Default for this property is false
             var property = this.GetStyleCopRuleProperty<BooleanProperty>("IgnorePrivates");
@@ -67,7 +86,7 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
             this.CompanyName = stringProperty != null ? stringProperty.Value : string.Empty;
 
             stringProperty = this.GetStyleCopRuleProperty<StringProperty>("Copyright");
-            this.Copyright = stringProperty != null ? stringProperty.Value : string.Empty;
+            this.Copyright = stringProperty != null ? this.ReplaceTokenVariables(stringProperty.Value) : string.Empty;
         }
 
         #endregion
@@ -160,6 +179,54 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
         #region Methods
 
         /// <summary>
+        /// Replaces any tokenised strings and returns the expanded result.
+        /// </summary>
+        /// <param name="value">The tokenised string to expand.</param>
+        /// <returns>The expanded string.</returns>
+        private string ReplaceTokenVariables(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            var creationTime = DateTime.Now;
+            var projectName = string.Empty;
+            var nowDate = DateTime.Now.Date;
+
+            var projectFile = this.file.ToProjectFile();
+
+            if (projectFile != null)
+            {
+                creationTime = projectFile.Location.ToFileInfo().CreationTime;
+                var project = projectFile.GetProject();
+                if (project != null)
+                {
+                    projectName = project.Name;
+                }
+            }
+            
+            var stringDictionary = new Dictionary<string, string>
+                {
+                    { "$USER_LOGIN$", Environment.UserName },
+                    { "$USER_NAME$", FullUserNameMacro.GetDisplayUserName() },
+                    { "$SOLUTION$", this.file.GetSolution().Name },
+                    { "$PROJECT$", projectName },
+                    { "$FILENAME$", this.file.Name },
+                    { "$CURRENT_YEAR$", nowDate.ToString("yyyy") },
+                    { "$CURRENT_MONTH$", nowDate.ToString("MM") },
+                    { "$CURRENT_DAY$", nowDate.ToString("dd") },
+                    { "$CURRENT_TIME$", nowDate.ToString("t") },
+                    { "$CREATED_YEAR$", creationTime.ToString("yyyy") },
+                    { "$CREATED_MONTH$", creationTime.ToString("MM") },
+                    { "$CREATED_DAY$", creationTime.ToString("dd") },
+                    { "$CREATED_TIME$", creationTime.ToString("t") },
+                };
+
+            return Environment.ExpandEnvironmentVariables(stringDictionary.Keys.Aggregate(value, (current, key) => current.Replace(key, stringDictionary[key])));
+        }
+
+        /// <summary>
         /// Gets the PropertyValue for the PropertyName passed in.
         /// </summary>
         /// <param name="propertyName">
@@ -170,18 +237,7 @@ namespace StyleCop.ReSharper61.CodeCleanup.Rules
         /// </returns>
         private PropertyValue GetSetting(string propertyName)
         {
-            if (this.settings != null)
-            {
-                foreach (var addInProperty in this.settings.AnalyzerSettings)
-                {
-                    if (addInProperty.AddIn.Id == AnalyzerName)
-                    {
-                        return addInProperty[propertyName];
-                    }
-                }
-            }
-
-            return null;
+            return this.settings != null ? (from addInProperty in this.settings.AnalyzerSettings where addInProperty.AddIn.Id == AnalyzerName select addInProperty[propertyName]).FirstOrDefault() : null;
         }
 
         #endregion
