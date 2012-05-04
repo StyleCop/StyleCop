a43dfce4c9c54a108342d753b32b09da6ee842af
Andy Reeves <devnull@localhost>
2012-05-04 15:17:17 +0100

    Update docs

--- Project/Docs/Rules/script/StandardText.js
+++ Project/Docs/Rules/script/StandardText.js
@@ -15,7 +15,7 @@ function WriteStyleCopNameAndVersion()
 
 function WritePageTop(title)
 {
-    document.write("<div id='pagetop'><table width='100%'><tr><td align='left'><span id='projecttitle'>" + StyleCopNameAndVersion() + "</span></td><td align='right'><span id='feedbacklink'><a target='_new' href='http://stylecop.codeplex.com/WorkItem/Create.aspx'>Is this doc correct?. Click here to open a bug.</a></span></td></tr><tr><td align='left' colspan='2'><span id='pagetitle'>" + title + "</span></td></tr></table></div>");
+    document.write("<div id='pagetop'><table width='100%'><tr><td align='left'><span id='projecttitle'>" + StyleCopNameAndVersion() + "</span></td><td align='right'><span id='feedbacklink'><a target='_new' href='http://stylecop.codeplex.com/WorkItem/Create.aspx'>Is this doc correct? Click here to open a bug.</a></span></td></tr><tr><td align='left' colspan='2'><span id='pagetitle'>" + title + "</span></td></tr></table></div>");
     return;
 }
 
