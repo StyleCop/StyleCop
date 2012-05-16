7d782c29e929b7f20f3700a5135a0c81d7525d0d
Andy Reeves <devnull@localhost>
2012-05-16 01:36:02 +0100

    Styling fixes

--- Project/Test/StyleCopCore/ObjectBasedEnvironmentTest/Properties/AssemblyInfo.cs
+++ Project/Test/StyleCopCore/ObjectBasedEnvironmentTest/Properties/AssemblyInfo.cs
@@ -1,5 +1,13 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
