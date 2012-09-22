c13bc6f4eb1e2f65b816d69c85abe7f85ca813b5
Andy Reeves <devnull@localhost>
2012-09-22 19:14:38 +0100

    Styling fixes.

--- Project/Src/AddIns/CSharp/Analyzers/CommentVerifier.cs
+++ Project/Src/AddIns/CSharp/Analyzers/CommentVerifier.cs
@@ -12,151 +12,35 @@
 //   notice, or any other, from this software.
 // </license>
 //-----------------------------------------------------------------------
+
 namespace StyleCop.CSharp
 {
-    using System.Globalization;
+    using System.Collections.Generic;
     using System.Text;
     using System.Xml;
 
     using StyleCop.Spelling;
-    
+
     /// <summary>
     /// Contains helper methods for verifying the validity and style of comments.
     /// </summary>
     internal static class CommentVerifier
     {
-        #region Internal Constants
-
-        /// <summary>
-        /// The minimum length for a valid comment.
-        /// </summary>
-        internal const int MinimumHeaderCommentLength = 10;
+        #region Constants
 
         /// <summary>
         /// The minimum number of characters in a valid comment.
         /// </summary>
         internal const int MinimumCharacterPercentage = 40;
 
-        #endregion Internal Constants
-
-        #region Public Static Methods
-
         /// <summary>
-        /// Checks the contents of the given comment string to determine whether the comment appears
-        /// to be a valid English-language sentence, or whether it appears to be garbage.
+        /// The minimum length for a valid comment.
         /// </summary>
-        /// <param name="commentWithAttributesRemoved">The comment to check (which has had its attributes removed).</param>
-        /// <param name="commentWithAttributesPreserved">The comment to check with attribute values inserted into the text.</param>
-        /// <param name="element">The element containing the text we're checking.</param>
-        /// <param name="spellingError">Returns the first word encountered as a spelling error.</param>
-        /// <returns>Returns the type of the comment.</returns>
-        public static InvalidCommentType IsGarbageComment(string commentWithAttributesRemoved, string commentWithAttributesPreserved, CsElement element, out string spellingError)
-        {
-            Param.AssertNotNull(commentWithAttributesRemoved, "commentWithAttributesRemoved");
-            Param.AssertNotNull(commentWithAttributesPreserved, "commentWithAttributesPreserved");
-            spellingError = null;
+        internal const int MinimumHeaderCommentLength = 10;
 
-            InvalidCommentType invalid = InvalidCommentType.Valid;
+        #endregion
 
-            string trimmedCommentWithoutAttributes = commentWithAttributesRemoved.Trim();
-
-            string trimmedCommentWithAttributesPreserved = commentWithAttributesPreserved.Trim();
-
-            string trimmedCommentWithAttributesPreservedWithoutPeriod = trimmedCommentWithAttributesPreserved.TrimEnd(new[] { '.' }).Trim();
-
-            // Make sure the comment string is valid.
-            if (string.IsNullOrEmpty(trimmedCommentWithAttributesPreserved))
-            {
-                invalid |= InvalidCommentType.Empty;
-            }
-            else
-            {
-                // Check the comment spelling
-                if (TextContainsIncorectSpelling(element, trimmedCommentWithoutAttributes, out spellingError))
-                {
-                    invalid |= InvalidCommentType.IncorrectSpelling;
-                }
-
-                // Check for the minimum length.
-                if (trimmedCommentWithAttributesPreserved.Length < MinimumHeaderCommentLength)
-                {
-                    invalid |= InvalidCommentType.TooShort;
-                }
-
-                // Verify that the comment starts with a capital letter.
-                if (!char.IsUpper(trimmedCommentWithAttributesPreserved[0]) && !char.IsDigit(trimmedCommentWithAttributesPreserved[0]))
-                {
-                    invalid |= InvalidCommentType.NoCapitalLetter;
-                }
-
-                // Verify that the comment ends with a period.
-                if (trimmedCommentWithAttributesPreserved[trimmedCommentWithAttributesPreserved.Length - 1] != '.')
-                {
-                    invalid |= InvalidCommentType.NoPeriod;
-                }
-
-                // Verify that at least 40% of the characters in the comment are letters, and that the comment contains
-                // at least one space between words.
-                float charCount = 0;
-                float nonCharCount = 0;
-                bool space = false;
-
-                foreach (char character in trimmedCommentWithAttributesPreservedWithoutPeriod)
-                {
-                    if (char.IsLetter(character))
-                    {
-                        ++charCount;
-                    }
-                    else
-                    {
-                        if (char.IsWhiteSpace(character))
-                        {
-                            space = true;
-                        }
-                        else
-                        {
-                            ++nonCharCount;
-                        }
-                    }
-                }
-
-                if (charCount == 0 || ((charCount / (charCount + nonCharCount)) * 100) < MinimumCharacterPercentage)
-                {
-                    invalid |= InvalidCommentType.TooFewCharacters;
-                }
-
-                if (!space)
-                {
-                    invalid |= InvalidCommentType.NoWhitespace;
-                }
-            }
-
-            return invalid;
-        }
-        
-        /// <summary>
-        /// Checks the contents of the given comment string to determine whether the comment appears
-        /// to be a valid English-language sentence, or whether it appears to be garbage.
-        /// </summary>
-        /// <param name="commentXml">The comment to check.</param>
-        /// <param name="element">The element containing the text we're checking.</param>
-        /// <param name="spellingError">Returns the first word encountered as a spelling error.</param>
-        /// <returns>Returns the type of the comment.</returns>
-        public static InvalidCommentType IsGarbageComment(XmlNode commentXml, CsElement element, out string spellingError)
-        {
-            Param.AssertNotNull(commentXml, "commentXml");
-
-            string commentWithAttributesRemoved = commentXml.InnerText;
-            string commentWithAttributesPreserved = commentXml.InnerText;
-
-            if (commentXml.HasChildNodes &&
-                (commentXml.ChildNodes.Count > 1 || commentXml.ChildNodes[0].NodeType != XmlNodeType.Text))
-            {
-                ExtractTextFromCommentXml(commentXml, out commentWithAttributesRemoved, out commentWithAttributesPreserved);
-            }
-
-            return IsGarbageComment(commentWithAttributesRemoved, commentWithAttributesPreserved, element, out spellingError);
-        }
+        #region Public Methods and Operators
 
         /// <summary>
         /// Extracts text from a comment Xml, including special values in attributes.
@@ -241,6 +125,127 @@ namespace StyleCop.CSharp
             textWithAttributesPreserved = commentWithAttributesPreservedBuilder.ToString().Trim();
         }
 
+        /// <summary>
+        /// Checks the contents of the given comment string to determine whether the comment appears
+        /// to be a valid English-language sentence, or whether it appears to be garbage.
+        /// </summary>
+        /// <param name="commentWithAttributesRemoved">The comment to check (which has had its attributes removed).</param>
+        /// <param name="commentWithAttributesPreserved">The comment to check with attribute values inserted into the text.</param>
+        /// <param name="element">The element containing the text we're checking.</param>
+        /// <param name="spellingError">Returns the first word encountered as a spelling error.</param>
+        /// <returns>Returns the type of the comment.</returns>
+        public static InvalidCommentType IsGarbageComment(
+            string commentWithAttributesRemoved, string commentWithAttributesPreserved, CsElement element, out string spellingError)
+        {
+            Param.AssertNotNull(commentWithAttributesRemoved, "commentWithAttributesRemoved");
+            Param.AssertNotNull(commentWithAttributesPreserved, "commentWithAttributesPreserved");
+            spellingError = null;
+
+            InvalidCommentType invalid = InvalidCommentType.Valid;
+
+            string trimmedCommentWithoutAttributes = commentWithAttributesRemoved.Trim();
+
+            string trimmedCommentWithAttributesPreserved = commentWithAttributesPreserved.Trim();
+
+            string trimmedCommentWithAttributesPreservedWithoutPeriod = trimmedCommentWithAttributesPreserved.TrimEnd(new[] { '.' }).Trim();
+
+            // Make sure the comment string is valid.
+            if (string.IsNullOrEmpty(trimmedCommentWithAttributesPreserved))
+            {
+                invalid |= InvalidCommentType.Empty;
+            }
+            else
+            {
+                // Check the comment spelling
+                if (TextContainsIncorectSpelling(element, trimmedCommentWithoutAttributes, out spellingError))
+                {
+                    invalid |= InvalidCommentType.IncorrectSpelling;
+                }
+
+                // Check for the minimum length.
+                if (trimmedCommentWithAttributesPreserved.Length < MinimumHeaderCommentLength)
+                {
+                    invalid |= InvalidCommentType.TooShort;
+                }
+
+                // Verify that the comment starts with a capital letter.
+                if (!char.IsUpper(trimmedCommentWithAttributesPreserved[0]) && !char.IsDigit(trimmedCommentWithAttributesPreserved[0]))
+                {
+                    invalid |= InvalidCommentType.NoCapitalLetter;
+                }
+
+                // Verify that the comment ends with a period.
+                if (trimmedCommentWithAttributesPreserved[trimmedCommentWithAttributesPreserved.Length - 1] != '.')
+                {
+                    invalid |= InvalidCommentType.NoPeriod;
+                }
+
+                // Verify that at least 40% of the characters in the comment are letters, and that the comment contains
+                // at least one space between words.
+                int charCount = 0;
+                int nonCharCount = 0;
+                bool space = false;
+
+                foreach (char character in trimmedCommentWithAttributesPreservedWithoutPeriod)
+                {
+                    if (char.IsLetter(character))
+                    {
+                        ++charCount;
+                    }
+                    else
+                    {
+                        if (char.IsWhiteSpace(character))
+                        {
+                            space = true;
+                        }
+                        else
+                        {
+                            ++nonCharCount;
+                        }
+                    }
+                }
+
+                if (charCount == 0 || (((charCount * 100) / (charCount + nonCharCount)) < MinimumCharacterPercentage))
+                {
+                    invalid |= InvalidCommentType.TooFewCharacters;
+                }
+
+                if (!space)
+                {
+                    invalid |= InvalidCommentType.NoWhitespace;
+                }
+            }
+
+            return invalid;
+        }
+
+        /// <summary>
+        /// Checks the contents of the given comment string to determine whether the comment appears
+        /// to be a valid English-language sentence, or whether it appears to be garbage.
+        /// </summary>
+        /// <param name="commentXml">The comment to check.</param>
+        /// <param name="element">The element containing the text we're checking.</param>
+        /// <param name="spellingError">Returns the first word encountered as a spelling error.</param>
+        /// <returns>Returns the type of the comment.</returns>
+        public static InvalidCommentType IsGarbageComment(XmlNode commentXml, CsElement element, out string spellingError)
+        {
+            Param.AssertNotNull(commentXml, "commentXml");
+
+            string commentWithAttributesRemoved = commentXml.InnerText;
+            string commentWithAttributesPreserved = commentXml.InnerText;
+
+            if (commentXml.HasChildNodes && (commentXml.ChildNodes.Count > 1 || commentXml.ChildNodes[0].NodeType != XmlNodeType.Text))
+            {
+                ExtractTextFromCommentXml(commentXml, out commentWithAttributesRemoved, out commentWithAttributesPreserved);
+            }
+
+            return IsGarbageComment(commentWithAttributesRemoved, commentWithAttributesPreserved, element, out spellingError);
+        }
+
+        #endregion
+
+        #region Methods
+
         private static void AddAttributeValue(StringBuilder commentWithAttributesBuilder, XmlNode childNode, string attributeName)
         {
             if (childNode.Attributes != null)
@@ -253,18 +258,28 @@ namespace StyleCop.CSharp
             }
         }
 
-        #endregion Public Static Methods
+        /// <summary>
+        /// Returns true if the word is spelled correctly.
+        /// </summary>
+        /// <param name="namingService">The naming service to use.</param>
+        /// <param name="word">The word to check.</param>
+        /// <returns>True if spelled correct.</returns>
+        private static bool IsSpelledCorrectly(NamingService namingService, string word)
+        {
+            return (namingService.GetPreferredAlternateForDeprecatedWord(word) != null) || (namingService.GetCompoundAlternateForDiscreteWord(word) != null)
+                   || (namingService.CheckSpelling(word) != WordSpelling.Unrecognized);
+        }
 
         /// <summary>
         /// Returns True if the text has incorrect spelling.
         /// </summary>
         /// <param name="element">The element containing the text we're checking.</param>
         /// <param name="text">The text to check.</param>
-        /// <param name="spellingError">Returns a comma seperated list of words encountered as spelling errors.</param>
+        /// <param name="spellingError">Returns a comma separated list of words encountered as spelling errors.</param>
         /// <returns>True if the text contains an incorrect spelling.</returns>
         private static bool TextContainsIncorectSpelling(CsElement element, string text, out string spellingError)
         {
-            var namingService = NamingService.GetNamingService(element.Document.SourceCode.Project.Culture);
+            NamingService namingService = NamingService.GetNamingService(element.Document.SourceCode.Project.Culture);
 
             spellingError = string.Empty;
 
@@ -273,7 +288,7 @@ namespace StyleCop.CSharp
                 WordParser parser = new WordParser(text, WordParserOptions.SplitCompoundWords);
                 if (parser.PeekWord() != null)
                 {
-                    var recognizedWords = element.Document.SourceCode.Project.RecognizedWords;
+                    ICollection<string> recognizedWords = element.Document.SourceCode.Project.RecognizedWords;
                     string word = parser.NextWord();
                     do
                     {
@@ -299,15 +314,6 @@ namespace StyleCop.CSharp
             return !string.IsNullOrEmpty(spellingError);
         }
 
-        /// <summary>
-        /// Returns true if the word is spelled correctly.
-        /// </summary>
-        /// <param name="namingService">The naming service to use.</param>
-        /// <param name="word">The word to check.</param>
-        /// <returns>True if spelled correct.</returns>
-        private static bool IsSpelledCorrectly(NamingService namingService, string word)
-        {
-            return (namingService.GetPreferredAlternateForDeprecatedWord(word) != null) || (namingService.GetCompoundAlternateForDiscreteWord(word) != null) || (namingService.CheckSpelling(word) != WordSpelling.Unrecognized);
-        }
+        #endregion
     }
 }
