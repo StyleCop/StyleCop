f5ccf2e0e729c883bdf58e96e3517c481e14ff52
Andy Reeves <devnull@localhost>
2012-09-23 21:52:13 +0100

    Moving StringExtensions to common area.

--- Project/Src/StyleCop/Extensions/StringExtensions.cs
+++ Project/Src/StyleCop/Extensions/StringExtensions.cs
@@ -18,6 +18,7 @@ namespace System
     #region Using Directives
 
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using System.Text.RegularExpressions;
 
@@ -50,7 +51,68 @@ namespace System
             chars[0] = char.ToUpperInvariant(chars[0]);
             return new string(chars);
         }
-        
+
+        /// <summary>
+        /// Replaces the format item in a <see cref="string"/> with the text equivalent of the value of a corresponding <see cref="object"/> 
+        /// instance in a specified array, using the <see cref="CultureInfo.InvariantCulture"/>.
+        /// </summary>
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// <param name="args">
+        /// An <see cref="object"/> array containing zero or more objects to format.
+        /// </param>
+        /// <returns>
+        /// A copy of format in which the format items have been replaced by the <see cref="string"/> equivalent of the corresponding instances 
+        /// of <see cref="object"/>  in args.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Either <paramref name="format"/> or <paramref name="args"/> is <c>Null.</c>.
+        /// </exception>
+        /// <exception cref="FormatException">
+        /// <paramref name="format"/>is invalid.
+        /// <para>
+        /// - or -.
+        /// </para>
+        /// The number indicating an argument to format is less than zero, or greater than or equal to the length of the <paramref name="args"/> array.
+        /// </exception>
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(CultureInfo.InvariantCulture, format, args);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a <see cref="string"/> with the text equivalent of the value of a corresponding <see cref="object"/> 
+        /// instance in a specified array. A specified parameter supplies culture-specific formatting information.
+        /// </summary>
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// <param name="provider">
+        /// An <see cref="IFormatProvider"/> that supplies culture-specific formatting information.
+        /// </param>
+        /// <param name="args">
+        /// An <see cref="object"/> array containing zero or more objects to format.
+        /// </param>
+        /// <returns>
+        /// A copy of format in which the format items have been replaced by the <see cref="string"/> equivalent of the corresponding instances 
+        /// of <see cref="object"/>  in args.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Either <paramref name="format"/> or <paramref name="args"/> is <c>Null.</c>.
+        /// </exception>
+        /// <exception cref="FormatException">
+        /// <paramref name="format"/>is invalid.
+        /// <para>
+        /// - or -.
+        /// </para>
+        /// The number indicating an argument to format is less than zero, or greater than or equal to the length of the <paramref name="args"/> array.
+        /// </exception>
+        public static string FormatWith(this string format, IFormatProvider provider, params object[] args)
+        {
+            return string.Format(provider, format, args);
+        }
+
         /// <summary>
         /// Indicates whether the specified <see cref="string"/> is null or an <see cref="string.Empty"/> string.
         /// </summary>
