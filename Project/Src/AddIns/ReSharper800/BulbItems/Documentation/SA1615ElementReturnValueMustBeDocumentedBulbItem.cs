264b0c2c78e77ebef85733da147794018b512cc5
Andy Reeves <devnull@localhost>
2013-07-25 18:16:46 +0100

    Add first draft of R#8 support

---
+++ Project/Src/AddIns/ReSharper800/BulbItems/Documentation/SA1615ElementReturnValueMustBeDocumentedBulbItem.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SA1615ElementReturnValueMustBeDocumentedBulbItem.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   BulbItem - SA1615ElementReturnValueMustBeDocumentedBulbItem : Inserts a return into the header.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.ReSharper800.BulbItems.Documentation
+{
+    #region Using Directives
+
+    using JetBrains.ProjectModel;
+    using JetBrains.ReSharper.Psi.CSharp.Tree;
+    using JetBrains.ReSharper.Psi.Tree;
+    using JetBrains.TextControl;
+
+    using StyleCop.ReSharper800.BulbItems.Framework;
+    using StyleCop.ReSharper800.CodeCleanup.Rules;
+    using StyleCop.ReSharper800.Core;
+
+    #endregion
+
+    /// <summary>
+    /// BulbItem - SA1615ElementReturnValueMustBeDocumentedBulbItem : Inserts a return into the header.
+    /// </summary>
+    internal class SA1615ElementReturnValueMustBeDocumentedBulbItem : V5BulbItemImpl
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The execute transaction inner.
+        /// </summary>
+        /// <param name="solution">
+        /// The solution.
+        /// </param>
+        /// <param name="textControl">
+        /// The text control.
+        /// </param>
+        public override void ExecuteTransactionInner(ISolution solution, ITextControl textControl)
+        {
+            ITreeNode element = Utils.GetElementAtCaret(solution, textControl);
+
+            IMethodDeclaration memberDeclaration = element.GetContainingNode<IMethodDeclaration>(true);
+            if (memberDeclaration != null)
+            {
+                new DocumentationRules().InsertReturnsElement(memberDeclaration, Utils.GetXmlPresentableName(memberDeclaration.DeclaredElement.ReturnType));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Project/Src/AddIns/ReSharper800/QuickFixes/Documentation/SA1638QuickFix.cs
@@ -0,0 +1,118 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SA1638QuickFix.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   QuickFix - SA1638: FileHeaderMustContainFileName.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.ReSharper800.QuickFixes.Documentation
+{
+    #region Using Directives
+
+    using System.Collections.Generic;
+
+    using JetBrains.ReSharper.Feature.Services.Bulbs;
+    using JetBrains.ReSharper.Intentions.Extensibility;
+
+    using StyleCop.ReSharper800.BulbItems.Documentation;
+    using StyleCop.ReSharper800.QuickFixes.Framework;
+    using StyleCop.ReSharper800.Violations;
+
+    #endregion
+
+    /// <summary>
+    ///   QuickFix - SA1638: FileHeaderMustContainFileName.
+    /// </summary>
+    //// [ShowQuickFix]
+    [QuickFix]
+    public class SA1638QuickFix : StyleCopQuickFixBase
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the SA1638QuickFix class that can handle <see cref="StyleCopHighlightingError"/> .
+        /// </summary>
+        /// <param name="highlight">
+        /// <see cref="StyleCopHighlightingError"/> that has been detected. 
+        /// </param>
+        public SA1638QuickFix(StyleCopHighlightingError highlight)
+            : base(highlight)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the SA1638QuickFix class that can handle <see cref="StyleCopHighlightingHint"/> .
+        /// </summary>
+        /// <param name="highlight">
+        /// <see cref="StyleCopHighlightingHint"/> that has been detected. 
+        /// </param>
+        public SA1638QuickFix(StyleCopHighlightingHint highlight)
+            : base(highlight)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the SA1638QuickFix class that can handle <see cref="StyleCopHighlightingInfo"/> .
+        /// </summary>
+        /// <param name="highlight">
+        /// <see cref="StyleCopHighlightingInfo"/> that has been detected. 
+        /// </param>
+        public SA1638QuickFix(StyleCopHighlightingInfo highlight)
+            : base(highlight)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the SA1638QuickFix class that can handle <see cref="StyleCopHighlightingSuggestion"/> .
+        /// </summary>
+        /// <param name="highlight">
+        /// <see cref="StyleCopHighlightingSuggestion"/> that has been detected. 
+        /// </param>
+        public SA1638QuickFix(StyleCopHighlightingSuggestion highlight)
+            : base(highlight)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the SA1638QuickFix class that can handle <see cref="StyleCopHighlightingWarning"/> .
+        /// </summary>
+        /// <param name="highlight">
+        /// <see cref="StyleCopHighlightingWarning"/> that has been detected. 
+        /// </param>
+        public SA1638QuickFix(StyleCopHighlightingWarning highlight)
+            : base(highlight)
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///   Initializes the QuickFix with all the available BulbItems that can fix the current StyleCop Violation.
+        /// </summary>
+        protected override void InitialiseBulbItems()
+        {
+            this.BulbItems = new List<IBulbAction>
+                                 {
+                                     new SA1637FileHeaderMustContainFileNameBulbItem
+                                         {
+                                             Description =
+                                                 "Update filename in header : " + this.Highlighting.ToolTip
+                                         }
+                                 };
+        }
+
+        #endregion
+    }
+}
