0a3dd39f2b65ade7799eceff3d1478128607b7e9
Chris Dahlberg <csdahlberg@gmail.com>
2016-01-16 20:50:51 -0600

    Added an example temporary generated file used in unit tests (which was omitted in a previous commit).

---
+++ Project/Test/AddIns/CSharp/Parser/CSharpParserTest/TestData/GeneratedCode/TemporaryGeneratedFile_5937a670_0e60_4077_877b_f7221da3dda1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+	 
+namespace TemporaryGeneratedFile_5937a670_0e60_4077_877b_f7221da3dda1.cs
+{
+    class ClassMissingAccessModifier
+    {
+    }
+}
