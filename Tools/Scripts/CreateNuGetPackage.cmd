4b6462627defc117f9a0406759667e0d239e900e
Chris Dahlberg <csdahlberg@gmail.com>
2016-03-13 21:36:20 -0500

    Renamed StyleCop.ReSharper92.nuspec to StyleCop.ReSharper920.nuspec and updated some properties.

--- Tools/Scripts/CreateNuGetPackage.cmd
+++ Tools/Scripts/CreateNuGetPackage.cmd
@@ -1,2 +1,2 @@
 CALL %STTOOLS%\NuGet\NuGet.exe pack %PROJECTROOT%\StyleCop.nuspec -o %1 -BasePath %1 -Version %2
-CALL %STTOOLS%\NuGet\NuGet.exe pack %PROJECTROOT%\StyleCop.Resharper92.nuspec -o %1 -BasePath %1
+CALL %STTOOLS%\NuGet\NuGet.exe pack %PROJECTROOT%\StyleCop.Resharper920.nuspec -o %1 -BasePath %1
