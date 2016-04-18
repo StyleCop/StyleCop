331e95272d4f3b17ff68055f7b0a235ebe9a2bf4
Chris Dahlberg <csdahlberg@gmail.com>
2016-04-18 17:09:41 -0500

    Added a ReSharper1000 project based off of the ReSharper920 project.

---
+++ Project/Src/AddIns/ReSharper1000/ZoneMarker.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ZoneMarker.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   The ZoneMarker to allow resharper to load this plugin.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.ReSharper1000
+{
+    using JetBrains.Application.BuildScript.Application.Zones;
+
+    /// <summary>
+    /// A dummy ZoneMarker class that allows this plugin to load into ReSharper.
+    /// </summary>
+    [ZoneMarker]
+    public class ZoneMarker
+    {
+    }
+}
