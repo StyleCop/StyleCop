b9ae833b0e3e16629ba805284390712f20529c57
Chris Dahlberg <csdahlberg@gmail.com>
2016-09-24 10:32:08 -0500

    Moved Src/AddIns/CSharp/Parser/CSharpParser.csproj to Src/StyleCop.CSharp/StyleCop.CSharp.csproj.

---
+++ Project/Src/StyleCop.CSharp/Expressions/NameofExpression.cs
@@ -0,0 +1,44 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NameofExpression.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   An expression representing a nameof operation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.CSharp
+{
+    /// <summary>
+    /// An expression representing a nameof operation.
+    /// </summary>
+    /// <subcategory>expression</subcategory>
+    public sealed class NameofExpression : Expression
+    {
+        /// <summary>
+        /// Initializes a new instance of the NameofExpression class.
+        /// </summary>
+        /// <param name="tokens">
+        /// The list of tokens that form the expression.
+        /// </param>
+        /// <param name="name">
+        /// The type literal to get the name of.
+        /// </param>
+        internal NameofExpression(CsTokenList tokens, Expression name)
+            : base(ExpressionType.NameOf, tokens)
+        {
+            Param.AssertNotNull(tokens, "tokens");
+            Param.AssertNotNull(name, "name");
+            
+            this.AddExpression(name);
+        }
+    }
+}
---
+++ Project/Src/StyleCop.CSharp/Expressions/NullConditionExpression.cs
@@ -0,0 +1,96 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NullConditionExpression.cs" company="http://stylecop.codeplex.com">
+//   MS-PL
+// </copyright>
+// <license>
+//   This source code is subject to terms and conditions of the Microsoft 
+//   Public License. A copy of the license can be found in the License.html 
+//   file at the root of this distribution. If you cannot locate the  
+//   Microsoft Public License, please send an email to dlr@microsoft.com. 
+//   By using this source code in any fashion, you are agreeing to be bound 
+//   by the terms of the Microsoft Public License. You must not remove this 
+//   notice, or any other, from this software.
+// </license>
+// <summary>
+//   An expression representing a null condition operation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace StyleCop.CSharp
+{
+    /// <summary>
+    /// An expression representing a null condition operation.
+    /// </summary>
+    /// <subcategory>expression</subcategory>
+    public sealed class NullConditionExpression : Expression
+    {
+        #region Fields
+
+        /// <summary>
+        /// The left hand side of the expression.
+        /// </summary>
+        private readonly Expression leftHandSide;
+
+        /// <summary>
+        /// The right hand side of the expression.
+        /// </summary>
+        private readonly Expression rightHandSide;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the NullConditionExpression class.
+        /// </summary>
+        /// <param name="tokens">
+        /// The list of tokens that form the expression.
+        /// </param>
+        /// <param name="leftHandSide">
+        /// The left hand side of the expression.
+        /// </param>
+        /// <param name="rightHandSide">
+        /// The right hand side of the expression.
+        /// </param>
+        internal NullConditionExpression(CsTokenList tokens, Expression leftHandSide, Expression rightHandSide)
+            : base(ExpressionType.NullCoalescing, tokens)
+        {
+            Param.AssertNotNull(tokens, "tokens");
+            Param.AssertNotNull(leftHandSide, "leftHandSide");
+            Param.AssertNotNull(rightHandSide, "rightHandSide");
+
+            this.leftHandSide = leftHandSide;
+            this.rightHandSide = rightHandSide;
+
+            this.AddExpression(leftHandSide);
+            this.AddExpression(rightHandSide);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the left hand side of the expression.
+        /// </summary>
+        public Expression LeftHandSide
+        {
+            get
+            {
+                return this.leftHandSide;
+            }
+        }
+
+        /// <summary>
+        /// Gets the right hand side of the expression.
+        /// </summary>
+        public Expression RightHandSide
+        {
+            get
+            {
+                return this.rightHandSide;
+            }
+        }
+
+        #endregion
+    }
+}
