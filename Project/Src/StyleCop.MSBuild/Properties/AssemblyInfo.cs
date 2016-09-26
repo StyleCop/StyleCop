775863c5616d303034bf2f5b0aa244f510e56453
Chris Dahlberg <csdahlberg@gmail.com>
2016-09-25 20:22:05 -0500

    Updated AssemblyTitle and AssemblyProduct of StyleCop.MSBuild to be consistent with other projects.

--- Project/Src/StyleCop.MSBuild/Properties/AssemblyInfo.cs
+++ Project/Src/StyleCop.MSBuild/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StyleCop.MSBuild")]
+[assembly: AssemblyTitle("StyleCop for MSBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StyleCop.MSBuild")]
+[assembly: AssemblyProduct("StyleCop for MSBuild")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
