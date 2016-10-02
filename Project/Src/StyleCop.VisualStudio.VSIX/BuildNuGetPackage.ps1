91679fae6531b5459a0756d99c7893015f0ea6c4
Chris Dahlberg <csdahlberg@gmail.com>
2016-10-02 17:22:42 -0500

    Removed the StyleCop.MSBuild project and modified StyleCop.VisualStudio.VSIX to create the StyleCop.MSBuild NuGet package.

---
+++ Project/Src/StyleCop.VisualStudio.VSIX/BuildNuGetPackage.ps1
@@ -0,0 +1,23 @@
+﻿param (
+    [string]$nuspecPath,
+    [string]$outputDirectory,
+    [string]$buildConfiguration,
+    [string]$nugetExePath
+)
+
+$assembly = [Reflection.Assembly]::LoadFrom($outputDirectory + "StyleCop.dll")
+$customAttributes = [Reflection.CustomAttributeData]::GetCustomAttributes($assembly)
+
+foreach ($customAttribute in $customAttributes)
+{
+    if ($customAttribute.AttributeType -Eq [System.Reflection.AssemblyInformationalVersionAttribute])
+    {
+       $informationalVersion = $customAttribute.ConstructorArguments[0].Value
+
+       $properties = "-Properties Configuration=$buildConfiguration"
+
+       & "$nugetExePath" pack "$nuspecPath" -Tool -BasePath "$outputDirectory " -OutputDirectory "$outputDirectory " -Properties Configuration=$buildConfiguration -Version $informationalVersion
+
+       break;
+    }
+}
