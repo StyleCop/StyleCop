698ee8a63285da45be820b12efa43a8c2f8eced6
hemnstill <hemn.still@gmail.com>
2019-02-16 14:42:21 +0400

    Stub test for default expression

---
+++ Project/Test/Tests.StyleCop.CSharp/TestData/DefaultLiteralExpressions/DefaultLiteralExpressions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.StyleCop.CSharp.TestData.DefaultLiteralExpressions
+{
+  public class DefaultLiteralExpressions
+  {
+    public DefaultLiteralExpressions()
+    {
+      int id = default(int);
+    }
+  }
+}
