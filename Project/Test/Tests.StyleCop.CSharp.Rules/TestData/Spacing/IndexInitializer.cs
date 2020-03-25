0822fb0cc943d6be76963f2d7d0ee724e299c25b
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2020-03-25 14:28:02 +0100

    Detect dictionary array-like initializer subexpressions

---
+++ Project/Test/Tests.StyleCop.CSharp.Rules/TestData/Spacing/IndexInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.StyleCop.CSharp.Rules.TestData.Spacing
+{
+    class IndexInitializer
+    {
+        static void InitializeInt()
+        {
+            var dict = new Dictionary<int, People>
+            {
+                [ 1 ] = new People( ),
+            };
+        }
+
+        public void InitializeString()
+        {
+            var dictionary = new Dictionary<string, string> { [ "key" ] = "value" };
+        }
+
+        private Dictionary<int, People> peoples = new Dictionary<int, People>
+        {
+            [3]=new People(){Name="test"},
+            [ 5 ] = new People( ) { Name = "test" },
+        };
+    }
+}
